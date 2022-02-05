f4c057ed0cc932c378dc377f8ae2391e546d0cfd
Teodor Ande Elstad <teodor@andeelstad.com>
2022-02-06 00:39:46 +0100

    Translate TodoApi to F# directly

--- TodoApi/Program.fs
+++ TodoApi/Program.fs
@@ -1,10 +1,44 @@
+open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 
-let args = System.Environment.GetCommandLineArgs().[1..]
+let args = Environment.GetCommandLineArgs().[1..]
 let builder = WebApplication.CreateBuilder(args)
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer() |> ignore
+builder.Services.AddSwaggerGen() |> ignore
+
 let app = builder.Build()
 
-type Action<'a> = delegate of unit -> 'a
-app.MapGet("/", Action((fun () -> "Hello World!"))) |> ignore
+// Configure the HTTP request pipeline.
+if app.Environment.IsDevelopment() then
+    app.UseSwagger() |> ignore
+    app.UseSwaggerUI() |> ignore
+
+app.UseHttpsRedirection() |> ignore
+
+let summaries = [| "Freezing"; "Bracing"; "Chilly"; "Cool"; "Mild"; "Warm"; "Balmy"; "Hot"; "Sweltering"; "Scorching" |]
+
+type WeatherForecast(date: DateTime, temperatureC: int, summary: string) =
+  member _.Date = date
+  member _.TemperatureC = temperatureC
+  member _.Summary = temperatureC
+  member _.TemperatureF = 32 + (int)((float)temperatureC / 0.5556)
+
+type ToAction<'a> = delegate of unit -> 'a
+app.MapGet("/weatherforecast", ToAction((fun () ->
+    let forecast =
+        seq { 1 .. 5 }
+        |> Seq.map (fun index ->
+            WeatherForecast(
+                DateTime.Now.AddDays(index),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]))
+        |> Seq.toArray
+    forecast)))
+    .WithName("GetWeatherForecast") |> ignore
 
 app.Run()
